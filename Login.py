792e5147e9157dc47a1d9421595d04479776dca3
arnaav11 <87122819+arnaav11@users.noreply.github.com>
2021-08-31 12:12:35 +0000

    Update Login.py

--- Login.py
+++ Login.py
@@ -7,27 +7,46 @@ import UserApps
 usrnm = input('Enter your username: ')
 passw = gp.getpass('Enter you password(no display): ')
 
+# Opening the file
 File = open('data.dat', 'r+')
+
+# Creating a string for each line without the '\n' escape sequence
 filelist = File.readlines()
 fileline1 = filelist[0][0:len(filelist[0])-1]
 fileline2 = filelist[1][0:len(filelist[1])-1]
 fileline3 = filelist[2]
+
+# Creating a Dictionary for each line(representin the level of the perso in the company) string
 filedict = ast.literal_eval(fileline1)
 filedict2 = ast.literal_eval(fileline2)
 filedict3 = ast.literal_eval(fileline3)
 
 
+# Checking if the person is a key in the admin dictionary and the password is its value to check
+# if the person is an admin
 if usrnm in filedict2 and passw == filedict2[usrnm]:
-        print("You are an admin. You'll have access to all commands.")
+        print("\nYou are an admin. You'll have access to all commands.")
+        
+        # Running the 'adminc' function which I made as well WITHOUT specifying if_user
+        # so it takes False as default
         adminc()
+
+# Checking if the person is a key in the owner dictionary and the password is its value to check
+# if the person is an owner
 elif usrnm in filedict3 and passw == filedict3[usrnm]:
-        print("You are an owner. You'll have access to all commands.")
+        print("\nYou are an owner. You'll have access to all commands.")
+
+        # Running the 'adminc' function which I made as well with 'if_owner' set to True
         adminc(if_owner=True)
+
+# Checking if the person is a key in the admin dictionary and the password is its value to check
+# if the person is an user
 elif  usrnm in filedict and passw == filedict[usrnm]:
-        print('Login Successful!\n')
+        print('\nLogin Successful!\n')
+        # Running the 'usrapps' function which i made
         UserApps.usrapps()
 else:
     print('not allowed')
 
-
+# Closing the file
 File.close()
